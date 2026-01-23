9290aa4618f73c3034a8bb8863aa364277fb3219
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-22 23:41:56 -0600

    Update data.sql

--- 01_cancellation_rates/data.sql
+++ 01_cancellation_rates/data.sql
@@ -1,1 +1,92 @@
+/* 
+01. Cancellation Rates.
 
+From the following table of user IDs,
+actions, and dates, write a query to
+return the publication and cancellation
+rate for each user. */
+
+/* ORACLE. */
+/********************************************************************/
+CREATE TABLE USERS_01 (
+    USER_ID INTEGER,
+    ACTION  VARCHAR(9),
+    DATES   DATE
+);
+
+INSERT INTO USERS_01
+    WITH NAMES AS (
+        SELECT
+            1,
+            'start',
+            '01-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            1,
+            'cancel',
+            '02-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            2,
+            'start',
+            '03-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            2,
+            'publish',
+            '04-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'start',
+            '05-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'cancel',
+            '06-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'start',
+            '07-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'cancel',
+            '08-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            1,
+            'start',
+            '07-jan-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            1,
+            'publish',
+            '08-jan-20'
+        FROM
+            DUAL
+    )
+    SELECT
+        *
+    FROM
+        NAMES;
