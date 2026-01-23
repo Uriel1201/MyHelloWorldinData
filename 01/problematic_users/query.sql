aed0495656589c38baee209a3bd8952f1d4043bd
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-23 11:23:48 -0600

    Add SQL query to identify problematic users

--- 01/problematic_users/query.sql
+++ 01/problematic_users/query.sql
@@ -1,1 +1,19 @@
+/* ORACLE 23ai. */
+/********************************************************************/
 
+SELECT
+    ITS.PROBLEMATIC_USER
+FROM
+    USERS_01 MATCH_RECOGNIZE (
+        PARTITION BY USER_ID
+        ORDER BY
+            DATES
+        MEASURES
+            USER_ID AS PROBLEMATIC_USER
+        ONE ROW PER MATCH
+    PATTERN ( A B C D ) DEFINE
+        A AS A.ACTION = 'start',
+        B AS B.ACTION = 'cancel',
+        C AS C.ACTION = 'start',
+        D AS D.ACTION = 'cancel'
+    ) ITS;
