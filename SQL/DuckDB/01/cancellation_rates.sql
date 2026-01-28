1109f1b0229f61202104dd698e97c02b42b21f77
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-27 23:40:07 -0600

    Update and rename SQL/data.sql to SQL/DuckDB/01/cancellation_rates.sql

---
+++ SQL/DuckDB/01/cancellation_rates.sql
@@ -0,0 +1,30 @@
+WITH
+    DUCK_UPDATED AS (
+        SELECT
+            USER_ID,
+            ACTION,
+            STRFTIME(STRPTIME(DATES, '%d-%b-%y'), '%Y-%m-%d')::DATE AS DATES
+        FROM
+            'USERS_01'),
+    TOTALS AS (
+        SELECT
+            USER_ID,
+            SUM(IF(ACTION = 'start',1,0)) AS TOTAL_STARTS,
+            SUM(IF(ACTION = 'cancel',1,0)) AS TOTAL_CANCELS,
+            SUM(IF(ACTION = 'publish',1,0)) AS TOTAL_PUBLISHES
+        FROM
+            DUCK_UPDATED
+        GROUP BY
+            USER_ID)
+SELECT
+    USER_ID,
+    ROUND(TOTAL_PUBLISHES / NULLIF(TOTAL_STARTS,
+                                   0),
+          2) AS PUBLISH_RATE,
+    ROUND(TOTAL_CANCELS / NULLIF(TOTAL_STARTS,
+                                 0),
+          2) AS CANCEL_RATE
+FROM
+    TOTALS
+ORDER BY
+    1;
