768b631304caf0b4a2c2788feed1c0a3d548e0ab
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-28 16:27:27 -0600

    Update ArrowDuckQuery.jl

--- Julia/ArrowDuckQuery.jl
+++ Julia/ArrowDuckQuery.jl
@@ -8,7 +8,14 @@ module ArrowDuckQuery
     #*  get_ArrowDuckQuery:
     #** params:
     #****************************************************************
-    function get_ArrowDuckQuery(ArrowFilename::AbstractString, Query:String)::DuckDB.QueryResult
+    function get_ArrowDuckQuery(duck::DuckDB.DB, ArrowFilename::AbstractString, DuckQuery:AbstractString)::DuckDB.QueryResult
+
+        arrow_table = Arrow.Table(ArrowFilename)
+        query = get_query(DuckQuery)
+        table_name = get_table_name(query)
+        DuckDB.register_data_frame(duck, arrow_table, table_name)
+
+        return DBInterface.execute(duck, query)
 
     end
     #****************************************************************
