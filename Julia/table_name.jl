6f9165108f8bb3d1938d65d5de796152e6e8549e
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-28 12:45:08 -0600

    Update table_name.jl

--- Julia/table_name.jl
+++ Julia/table_name.jl
@@ -1,1 +1,20 @@
+module TableName
 
+export table_name
+
+#
+#
+
+function table_name(filename::AbstractString)::String
+
+    name = uppercase(table)
+  
+    query = open(filename) do file
+        read(file, String)
+    end
+  
+    table_name = match(r"FROM\s+'(\w+)'", query).captures[1]
+
+    return table_name
+
+end
