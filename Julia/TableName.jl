6919969e15e0016c47d7e9dd7a019af718a44e85
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-28 13:55:05 -0600

    Update TableName.jl

--- Julia/TableName.jl
+++ Julia/TableName.jl
@@ -8,13 +8,25 @@ module TableName
     #****************************************************************
     function get_table_name(filename::AbstractString)::String
 
+        if !isfile(filename)
+            throw(ArgumentError("'$filename' does not exist"))
+        end
+    
         query = open(filename) do file
             read(file, String)
         end
   
-        table_name = match(r"FROM\s+'(\w+)'", query).captures[1]
-
-        return table_name
+        m = match(r"FROM\s+'(\w+)'", query).captures[1]
+    
+        if m === nothing
+            throw(ArgumentError("""Unable to locate the 
+                                   table name in your SQL query 
+                                """
+                  )
+            )
+        end
+    
+        return m
 
     end
 
