b7545b39489856a325e6d839e3b65c022694b3e6
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-28 15:32:31 -0600

    Add QueryMeta.jl file

--- Julia/QueryMeta.jl
+++ Julia/QueryMeta.jl
@@ -1,21 +1,31 @@
-module TableName
+module QueryMeta
 
-    export get_table_name
-
+    export get_query, get_table_name
     #****************************************************************
-    #*  table_name:
-    #** filename:
+    #*  get_query:
+    #** params:
     #****************************************************************
-    function get_table_name(filename::AbstractString)::String
-
+    function get_query(filename::AbstractString)::String
+        
         if !isfile(filename)
             throw(ArgumentError("'$filename' does not exist"))
         end
-    
+
         query = open(filename) do file
             read(file, String)
         end
-  
+
+        return query
+    
+    end
+    #****************************************************************
+    
+    #****************************************************************
+    #*  get_table_name:
+    #** params:
+    #****************************************************************
+    function get_table_name(query::String)::String
+
         m = match(r"FROM\s+'(\w+)'", query).captures[1]
     
         if m === nothing
