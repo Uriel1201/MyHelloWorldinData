18ca517736c4bc87488d197112e6c967e172efe5
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-30 15:25:16 -0600

    Update ArrowDuckQuery.jl

--- Julia/Queries/ArrowDuckQuery.jl
+++ Julia/Queries/ArrowDuckQuery.jl
@@ -2,26 +2,21 @@ module ArrowDuckQuery
 
     using ..MetaQuery, Arrow, DuckDB
 
-    export get_ArrowDuckQuery
+    export get_DuckQuery
 
     #****************************************************************
-    #*  get_ArrowDuckQuery:
+    #*  get_DuckQuery:
     #** params:
     #****************************************************************
-    function get_ArrowDuckQuery(duck::DuckDB.DB, ArrowFilename::AbstractString, DuckQueryFilename::AbstractString)::DuckDB.QueryResult
-
-        if !isfile(ArrowFilename)
-            throw(ArgumentError("'$ArrowFilename' does not exist"))
-        end
+    function get_DuckQuery(duck::DuckDB.DB, ArrowTable::Arrow.Table, DuckQueryFilename::AbstractString)::DuckDB.QueryResult
 
         if !isfile(DuckQueryFilename)
             throw(ArgumentError("'$DuckQueryFilename' does not exist"))
         end
 
-        arrow_table = Arrow.Table(ArrowFilename)
         query = get_query(DuckQueryFilename)
         table_name = get_table_name(query)
-        DuckDB.register_data_frame(duck, arrow_table, table_name)
+        DuckDB.register_data_frame(duck, ArrowTable, table_name)
 
         return DBInterface.execute(duck, query)
 
