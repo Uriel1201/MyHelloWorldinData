dfa24a732a0aaa0c0aae7a2a605da7864e8a5b70
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-01-30 13:57:11 -0600

    Create users_01CancellationRates.sql

---
+++ SQL/SQLite/users_01CancellationRates.sql
@@ -0,0 +1,19 @@
+WITH
+    TOTALS AS (
+        SELECT
+            USER_ID,
+            1.0 * SUM(CASE WHEN ACTION = "start" THEN 1 ELSE 0 END) AS TOTAL_STARTS,
+            1.0 * SUM(CASE WHEN ACTION = "cancel" THEN 1 ELSE 0 END) AS TOTAL_CANCELS,
+            1.0 * SUM(CASE WHEN ACTION = "publish" THEN 1 ELSE 0 END) AS TOTAL_PUBLISHES
+        FROM
+            USERS_01
+        GROUP BY
+            USER_ID)
+      SELECT
+          USER_ID,
+          TOTAL_PUBLISHES / NULLIF(TOTAL_STARTS, 0) AS PUBLISH_RATE,
+          TOTAL_CANCELS / NULLIF(TOTAL_STARTS, 0) AS CANCEL_RATE
+      FROM
+          TOTALS
+      ORDER BY 
+          1;1
