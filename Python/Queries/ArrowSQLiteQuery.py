8bb56372e9b44619f22b48dabe681b5c94ff06d7
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-02-01 12:10:12 -0600

    Update ArrowSQLiteQuery.py

--- Python/Queries/ArrowSQLiteQuery.py
+++ Python/Queries/ArrowSQLiteQuery.py
@@ -37,3 +37,41 @@ def get_ArrowTable(conn:dbapi.AdbcSqliteConnection, table:str) -> pa.Table:
     except Exception as e:
 
         print(f'Corrupted query or table not available: {e}')
+    #****************************************************************
+    #*  get_query:
+    #** params:
+    #****************************************************************
+    function get_query(filename::AbstractString)::String
+        
+        if !isfile(filename)
+            throw(ArgumentError("'$filename' does not exist"))
+        end
+
+        query = open(filename) do file
+            read(file, String)
+        end
+
+        return query
+    
+    end
+    #****************************************************************
+    
+    #****************************************************************
+    #*  get_table_name:
+    #** params:
+    #****************************************************************
+    function get_table_name(query::String)::String
+
+        m = match(r"FROM\s+'(\w+)'", query).captures[1]
+    
+        if m === nothing
+            throw(ArgumentError("""Unable to locate the 
+                                   table name in your SQL query 
+                                """
+                  )
+            )
+        end
+    
+        return m
+
+    end
