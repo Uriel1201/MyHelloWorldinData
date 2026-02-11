9c99e1e2849c9851f6a7e497331849db6c08b17b
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-02-11 09:45:59 -0600

    Update and rename draft.py to Users01.py

---
+++ Python/DataFrames/Users01.py
@@ -0,0 +1,26 @@
+import polars as pl
+import pyarrow as pa
+
+def get_CancellationRates(table:pa.Table) -> pl.DataFrame:
+
+    pl_rates = (pl.from_arrow(table)
+                  .lazy()
+                  .group_by('USER_ID')
+                  .agg([pl.col('ACTION').filter(pl.col('ACTION')=='start').count().alias('STARTS'),
+                        pl.col('ACTION').filter(pl.col('ACTION')=='cancel').count().alias('CANCELS'),
+                        pl.col('ACTION').filter(pl.col('ACTION')=='publish').count().alias('PUBLISHES')
+                       ]
+                   )
+                  .select(pl.col('USER_ID'),
+                          pl.when(pl.col('STARTS') > 0)
+                            .then(pl.col('CANCELS') / pl.col('STARTS'))
+                            .otherwise(None)
+                            .alias('CANCEL_RATE'),
+                          pl.when(pl.col('STARTS') > 0)
+                            .then(pl.col('PUBLISHES') / pl.col('STARTS'))
+                            .otherwise(None)
+                            .alias('PUBLISH_RATE')
+                   )
+               )
+
+    return pl_rates.collect()
