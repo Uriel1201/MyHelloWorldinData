76859ff2815bf674c5f8d49f2937e3ecfbd0a785
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-02-13 12:15:00 -0600

    Update MetaQuery.jl

--- Julia/Queries/MetaQuery.jl
+++ Julia/Queries/MetaQuery.jl
@@ -1,10 +1,12 @@
 module MetaQuery
 
     export get_query, table_name
-    #****************************************************************
-    #*  get_Query:
-    #** params:
-    #****************************************************************
+
+    """
+        get_query(filename::AbstractString) -> String
+
+    Returns the SQL query from an SQL file as a String:
+    """
     function get_query(filename::AbstractString)::String
         
         if !isfile(filename)
@@ -18,12 +20,12 @@ module MetaQuery
         return query
     
     end
-    #****************************************************************
     
-    #****************************************************************
-    #*  get_TableName:
-    #** params:
-    #****************************************************************
+    """
+        table_name(query::String) -> String
+
+    Returns the table name from an SQL query:
+    """
     function table_name(query::String)::String
 
         m = match(r"FROM\s+'(\w+)'", query).captures[1]
