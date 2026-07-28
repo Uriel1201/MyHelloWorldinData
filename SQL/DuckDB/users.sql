78470f7793e6b83a6ba34969b4aa339eeac99706
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-07-28 08:25:46 -0600

    Create users.sql

---
+++ SQL/DuckDB/users.sql
@@ -0,0 +1,14 @@
+/* DUCKDB. */
+/********************************************************************/
+WITH DUCKTABLE AS (
+    SELECT
+        USER_ID,
+        ACTION,
+        STRFTIME(STRPTIME(DATES, '%d-%b-%y'), '%Y-%m-%d')::DATE AS DATES
+    FROM
+        sqlite_scan('MyDataBase.db', 'USERS_01')
+    )
+SELECT 
+    *
+FROM
+    DUCKTABLE;
