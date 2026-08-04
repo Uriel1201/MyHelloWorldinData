07b8c9a58b47c3a9e25588bb27e0f7d9d61b4813
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-04 02:50:37 -0600

    Export REGISTERED_TABLES and MyArrowTable

--- Julia/Config.jl
+++ Julia/Config.jl
@@ -1,9 +1,13 @@
 module Config
 
+    export REGISTERED_TABLES
+    export MyArrowTable 
+
+    const REGISTERED_TABLES = Set(["USERS_01"])
+    
     struct MyArrowTable
             name::String
             table::Arrow.Table
     end
-
 
 end
