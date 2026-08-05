47d0985bb24b6e660bb7ee3be1d23cbd5015c7f6
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-04 21:10:18 -0600

    Update Config.jl

--- Julia/Config.jl
+++ Julia/Config.jl
@@ -4,12 +4,22 @@ module Config
 
     export REGISTERED_TABLES
     export MyArrowTable 
+    export get_my_arrow_table
 
     const REGISTERED_TABLES = Set(["USERS_01"])
     
     struct MyArrowTable
             name::String
             table::Arrow.Table
+    end
+
+    function get_my_arrow_table(ArrowFile::AbstractString)::MyArrowTable
+
+        return Config.MyArrowTable(
+                   splitext(basename(ArrowFile))[1],
+                   Arrow.Table(ArrowFile)
+               )
+    
     end
 
 end
