0823c65169e8f11283eb63b740c14dd74011c38c
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-05 09:13:10 -0600

    Update DataPipeline.jl

--- Julia/database_setup/DataPipeline.jl
+++ Julia/database_setup/DataPipeline.jl
@@ -11,20 +11,4 @@ module DataPipeline
 
     MyDataBase.construct_database()
 
-    function print_data_analysis(ArrowFile::AbstractString, DuckFile::AbstractString)::Nothing
-
-        my_table = Config.get_my_arrow_table(ArrowFile)
-        duck = DBInterface.connect(DuckDB.DB, ":memory:")
-        if (my_table.name == "USERS_01")
-            println("USING JULIA DATAFRAMES TO GET CANCELLATION RATES FOR EACH USER")
-            println(MyDataFrames.cancellation_rates_01(my_table))
-        end
-
-        println("\nUSING DUCKDB TO PRINT THE CORRESPONDING QUERY")
-        query = MyQueries.get_duck_query(duck, my_table, "rates.sql")
-        MyQueries.print_duck_query(query)
-        DBInterface.close!(duck)
-
-    end
-
 end
