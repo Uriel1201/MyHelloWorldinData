48994b8b9e00aca3d253c581806dbf531fb8242a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-05 03:44:00 -0600

    Update DataPipeline.jl

--- Julia/database_setup/DataPipeline.jl
+++ Julia/database_setup/DataPipeline.jl
@@ -1,11 +1,29 @@
 module DataPipeline
 
-include("Extract/sqlite_to_arrow.jl")
-include("Analytics/duckdb_queries.jl")
-include("Analytics/dataframe_analysis.jl")
+    include("Config.jl")
+    include("MyDataBase.jl")
+    include("SQLtoArrow.jl")
+    include("MyQueries.jl")
+    include("MyDataFrames.jl")
 
-export create_arrow
-export run_duckdb_analysis
-export run_dataframe_analysis
+    using .MyDataBase, .Config, DBInterface, DuckDB, .MyQueries, .MyDataFrames 
+    import .SQLtoArrow: main as convert_file
+
+    MyDataBase.construct_database()
+
+    function print_data_analysis(ArrowFile::AbstractString, DuckFile::AbstractString)::Nothing
+
+        my_table = Config.get_my_arrow_table(ArrowFile)
+        duck = DBInterface.connect(DuckDB.DB, ":memory:")
+        if (my_table.name == "USERS_01")
+            println("USING JULIA DATAFRAMES TO GET CANCELLATION RATES FOR EACH USER")
+            println(MyDataFrames.cancellation_rates_01(my_table))
+        end
+
+        println("\nUSING DUCKDB TO PRINT THE CORRESPONDING QUERY")
+        query = MyQueries.get_duck_query(duck, my_table, "rates.sql")
+        MyQueries.print_duck_query(query)
+
+    end
 
 end
