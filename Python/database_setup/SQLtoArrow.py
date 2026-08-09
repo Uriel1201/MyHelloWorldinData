8a3f69a3ac492b587c13d8b1beccc01f5cc00d1a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-09 05:51:13 -0600

    Update SQLtoArrow.py

--- Python/database_setup/SQLtoArrow.py
+++ Python/database_setup/SQLtoArrow.py
@@ -1,3 +1,10 @@
+import adbc_driver_sqlite.dbapi as dbapi
+import pyarrow as pa
+
+# ============================================================
+# get_query:
+# params:
+# ============================================================
 def get_query(filename: str) -> str:
 
     try:
@@ -9,3 +16,31 @@ def get_query(filename: str) -> str:
     except FileNotFoundError:
         
         return f"Error:'{filename}' does not exist in directory."
+
+# ============================================================
+# sqlite_to_arrow:
+# params:
+# ============================================================
+def sqlite_to_arrow(Query:str, outputNameFile:str) -> None:
+
+    try:
+        with dbapi.connect("file:/content/MyDataBase.db?mode=ro").cursor() as cursor:
+          
+            batches = cursor.execute(Query).fetch_record_batch()
+            with pa.OSFile(f'{outputNameFile}.arrow', 'wb') as my_file:
+                with pa.ipc.new_file(my_file, batches.schema) as writer:
+                    for batch in batches:
+                        writer.write_batch(batch)
+
+    except Exception as e:
+
+        print(f'Corrupted query or table not available: {e}')
+
+# ============================================================
+# read_arrow_file:
+# params:
+# ============================================================
+def read_arrow_file(ArrowFile:str) -> pa.lib.Table:
+
+    with pa.memory_map(ArrowFile, 'rb') as source:
+        return pa.ipc.open_file(source).read_all()
