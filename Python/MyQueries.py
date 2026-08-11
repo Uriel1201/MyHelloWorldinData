faf2e4aa95d1da742e117bc5f2a8651ee4a89b88
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-11 06:36:45 -0600

    Update MyQueries.py

--- Python/MyQueries.py
+++ Python/MyQueries.py
@@ -1,5 +1,8 @@
 import duckdb
 import config
+import adbc_driver_sqlite.dbapi as dbapi
+import duckdb as duck
+import SQLtoArrow as sa
 
 # ============================================================
 # print_duck_query:
@@ -9,3 +12,20 @@ def print_duck_query(ArrowTable: config.MyArrowTable, DuckQuery: str) -> None:
 
     duckdb.register(ArrowTable.alias, ArrowTable.table)
     duckdb.sql(sa.get_query(DuckQuery)).show()
+
+# ============================================================
+# print_sqlite_query:
+# params:
+# ============================================================
+def print_sqlite_query(conn: dbapi.AdbcSqliteConnection, SQLiteQuery: str) -> None:
+ 
+    query = sa.get_query(SQLiteQuery)
+    with conn.cursor() as cursor:
+        cursor.adbc_statement.set_options(
+            **{
+                "adbc.sqlite.query.batch_rows": 1,
+            }
+        )
+        batches = cursor.execute(query).fetch_record_batch()
+        for batch in batches:
+            print(f'************\n{batch}\n************\n')
