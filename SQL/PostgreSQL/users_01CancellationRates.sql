5aa83fc60eb0059305fe055ff924a2e215b1e5e8
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-12 13:28:16 -0600

    Create users_01CancellationRates.sql

---
+++ SQL/PostgreSQL/users_01CancellationRates.sql
@@ -0,0 +1,48 @@
+with
+    "TOTALS" as (
+        select
+            "USER_ID",
+            sum(
+                case 
+                    when "ACTION" = 'start' 
+                    then 1 
+                    else 0 
+                end
+            )::numeric as "TOTAL_STARTS",
+            sum(
+                case 
+                    when "ACTION" = 'cancel' 
+                    then 1 
+                    else 0 
+                end
+            )::numeric as "TOTAL_CANCELS",
+            sum(
+                case 
+                    when "ACTION" = 'publish' 
+                    then 1 
+                    else 0 
+                end
+            )::numeric as "TOTAL_PUBLISHES"
+        from
+            "USERS_01"
+        group by
+            "USER_ID"
+    )
+select
+    "USER_ID",
+    round("TOTAL_PUBLISHES" / case 
+              when "TOTAL_STARTS" = 0 then null 
+              else "TOTAL_STARTS" 
+          end, 
+          3
+    ) as "PUBLISH_RATE",
+    round("TOTAL_CANCELS" / case 
+              when "TOTAL_STARTS" = 0 then null 
+              else "TOTAL_STARTS" 
+          end, 
+          3
+    ) as "CANCEL_RATE"
+FROM
+    "TOTALS"
+ORDER BY 
+    1;
