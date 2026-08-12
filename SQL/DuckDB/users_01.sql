f398e55adba9976a335aefec76f54a6b50dc2c29
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-12 10:51:41 -0600

    Update users_01.sql

--- SQL/DuckDB/users_01.sql
+++ SQL/DuckDB/users_01.sql
@@ -4,7 +4,7 @@ WITH DUCKTABLE AS (
     SELECT
         USER_ID,
         ACTION,
-        STRFTIME(STRPTIME(DATES, '%d-%b-%y'), '%Y-%m-%d')::DATE AS DATES
+        STRFTIME(DATES, '%Y-%m-%d')::DATE AS DATES
     FROM
         sqlite_scan('MyDataBase.db', 'USERS_01')
     )
